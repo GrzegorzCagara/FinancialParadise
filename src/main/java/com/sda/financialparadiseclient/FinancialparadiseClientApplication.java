f557a6da8efec9e01c4fb5e29fd8d93ed630f823
Rafal Ciesla <ra.ciesla@wp.pl>
2017-11-28 18:29:26 +0100

    initial commit

---
+++ src/main/java/com/sda/financialparadiseclient/FinancialparadiseClientApplication.java
@@ -0,0 +1,12 @@
+package com.sda.financialparadiseclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialparadiseClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialparadiseClientApplication.class, args);
+	}
+}

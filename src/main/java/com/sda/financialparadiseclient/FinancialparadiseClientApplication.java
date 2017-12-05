ca92375a5caae3449e300ef57e595169c66b9ac2
Grzegorz <cagaragrzegorz@gmail.com>
2017-12-05 20:35:01 +0100

    working version

--- src/main/java/com/sda/financialparadiseclient/FinancialparadiseClientApplication.java
+++ src/main/java/com/sda/financialparadiseclient/FinancialparadiseClientApplication.java
@@ -1,5 +1,6 @@
 package com.sda.financialparadiseclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,16 @@ public class FinancialparadiseClientApplication {
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
 	}
+
+    @Bean
+    public DriverManagerDataSource dataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+        driverManagerDataSource.setUrl("jdbc:postgresql://ec2-54-247-124-9.eu-west-1.compute.amazonaws.com:5432/df7stvvtr25t3i?sslmode=require");
+        driverManagerDataSource.setUsername("ypcfzaahcckamo");
+        driverManagerDataSource.setPassword("1b8a29b09cb5920eb4ae8049d867380e22e45f3a8153f845104e5289709fbd72");
+        return driverManagerDataSource;
+    }
 
 	@Bean(name = "restTemplate")
 	public RestTemplate getRestClient() {
--- src/main/java/com/sda/financialparadiseclient/config/SecurityConfiguration.java
+++ src/main/java/com/sda/financialparadiseclient/config/SecurityConfiguration.java
@@ -27,21 +27,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
 
 
-    @Bean
-    public JdbcTemplate jdbcTemplate(){
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public DriverManagerDataSource dataSource(){
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-        driverManagerDataSource.setUrl("jdbc:postgresql://ec2-54-247-124-9.eu-west-1.compute.amazonaws.com:5432/df7stvvtr25t3i?sslmode=require");
-        driverManagerDataSource.setUsername("ypcfzaahcckamo");
-        driverManagerDataSource.setPassword("1b8a29b09cb5920eb4ae8049d867380e22e45f3a8153f845104e5289709fbd72");
-        return driverManagerDataSource;
-    }
-
     /*@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication().withUser("adrian").password("adrian").roles("USER");
@@ -59,8 +44,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/actors")
-                .access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/customers/find/all")
+                .access("hasRole('ROLE_USER')")
                 .and().formLogin()
                 .and().csrf().disable();
     }

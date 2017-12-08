77c890742ce98bc2994e86148348797921a026e2
Moler <moler555@gmail.com>
2017-12-08 02:46:27 +0100

    security added

--- src/main/java/com/sda/financialparadiseclient/config/SecurityConfiguration.java
+++ src/main/java/com/sda/financialparadiseclient/config/SecurityConfiguration.java
@@ -1,44 +1,53 @@
 package com.sda.financialparadiseclient.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Controller
+public class SecurityConfiguration extends WebMvcConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
 
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("select email,password,enabled from users where email=?")
                 .authoritiesByUsernameQuery("select email, role from user_roles where email=?");
-
     }
 
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @EnableWebSecurity
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    class SecurityConfig extends WebSecurityConfigurerAdapter {
+
         @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/customers/*")
-                .access("hasRole('ROLE_USER')")
-                .antMatchers("/admin/*", "/customers/*")
-                .access("hasRole('ROLE_ADMIN')")
-                .and().formLogin()
-                .and().logout().logoutSuccessUrl("/")
-                .and().csrf().disable();
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/view/**").permitAll()
+                    .antMatchers("/customers/*")
+                    .access("hasRole('ROLE_USER')")
+                    .antMatchers("/admin/*", "/customers/*")
+                    .access("hasRole('ROLE_ADMIN')")
+                    .and().formLogin().loginPage("/login").permitAll()
+                    .and().logout().logoutSuccessUrl("/")
+                    .and().csrf().disable();
+        }
     }
 }
